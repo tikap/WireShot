f52293fd78229de3556f04be0a51dcf18d7e3eb2
tikap <tikapradhan095@gmail.com>
2024-03-17 01:18:18 -0400

    Bouncy circle init

--- wireshot.client/src/App.tsx
+++ wireshot.client/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
+import BouncyCircle from './animated/BouncyCircle';
 
 function App() {
     return (
-        <div>
-            <h1 id="tabelLabel">WireShot</h1>
-            <p>Animated communication protocols</p>
-        </div>
+        <>
+            <div>
+                <h1 id="tabelLabel">WireShot</h1>
+                <p>Animated communication protocols</p>
+            </div>
+
+            <BouncyCircle duration={5000} />
+        </>
     );
 }
 
