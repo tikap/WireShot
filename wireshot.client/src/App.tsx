9023bcccae43175a2756ce4ab829a1f9c35648a3
tikap <tikapradhan095@gmail.com>
2024-03-20 23:00:39 -0400

    Enough toying with Mo Js, clean slate

--- wireshot.client/src/App.tsx
+++ wireshot.client/src/App.tsx
@@ -1,17 +1,10 @@
 import './App.css';
-import BouncyCircle from './animated/BouncyCircle';
 
 function App() {
     return (
-        <div className="grid-container">
-            <div className="grid-item">
-                <h1 id="tabelLabel">WireShot</h1>
-                <p>Animated communication protocols</p>
-            </div>
-
-            <div className="grid-item">
-                <BouncyCircle duration={3000} />
-            </div>
+        <div>
+            <h1 id="tabelLabel">WireShot</h1>
+            <p>Animated communication protocols</p>
         </div>
     );
 }
